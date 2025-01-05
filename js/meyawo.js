d900e1e43bc4c2f8cbe5565baa231ceec41763b1
Agathe <agathemaud.dormoy@gmail.com>
2025-01-05 15:28:36 +0100

    rechercge

--- js/meyawo.js
+++ js/meyawo.js
@@ -36,29 +36,3 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
-
-
-
-document.getElementById('myemail').addEventListener('click', function () {
-    // Créer une zone de sélection temporaire
-    const range = document.createRange();
-    range.selectNode(this); // Sélectionne le contenu du paragraphe
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    // Copier le texte sélectionné
-    try {
-        const success = document.execCommand('copy');
-        if (success) {
-            alert("Texte copié : " + this.textContent);
-        } else {
-            alert("Échec de la copie.");
-        }
-    } catch (err) {
-        console.error("Erreur lors de la tentative de copie : ", err);
-    }
-
-    // Désélectionner après la copie
-    selection.removeAllRanges();
-});
